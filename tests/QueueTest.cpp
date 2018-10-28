225847d029e5cc58bf0114fb721f271c71df2cdf
Unknown <andrempmattos@gmail.com>
2018-10-27 21:31:23 -0300

    Add queue automated test

---
+++ tests/QueueTest.cpp
@@ -0,0 +1,28 @@
+#include "../firmware/Queue.hpp"
+
+#include <iostream>
+
+int main() {
+
+    using namespace std;
+    using namespace VMCore;
+
+    Queue queue_A;
+    int output;
+
+    queue_A.push(5);
+    cout << "5" << endl;
+    queue_A.push(10);
+    cout << "10" << endl;
+    queue_A.push(22);
+    cout << "22" << endl;
+
+    cout << queue_A.pop() << endl;
+    cout << queue_A.pop() << endl;
+    cout << queue_A.pop() << endl;
+
+    cin >> output;
+
+    return 0;
+}
+
