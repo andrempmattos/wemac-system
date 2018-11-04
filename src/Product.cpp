8b2b37111dd089a0589fbb8934cb2b99328698a7
Unknown <andrempmattos@gmail.com>
2018-11-04 05:43:36 -0200

    Add basic product database system

--- src/Product.cpp
+++ src/Product.cpp
@@ -2,15 +2,27 @@
 
 using namespace VMCore;
 
-std::string Product::getName() {
+Product::Product(void) {
+}
+
+
+int Product::getId(void) {
+	return m_id;	
+}
+
+void Product::setId(ProductID t_id) {
+	m_id = t_id;	
+}
+
+std::string Product::getName(void) {
 	return m_name;
 }
 
-float Product::getValue() {
+float Product::getValue(void) {
 	return m_value;
 }
 
-int Product::getStock() {
+int Product::getStock(void) {
 	return m_stock;
 }
 
@@ -24,4 +36,61 @@ void Product::setValue(float t_value) {
 
 void Product::setStock(int t_stock) {
 	m_stock = t_stock;
+}
+
+void Product::getProductDatabase(std::vector<productInfo> &pData) {
+	using namespace std;
+
+	string line;
+	string contentSeparator = ";";
+	string data[4];
+	int contentStartPos = 0;
+	int contentEndPos = 0;
+	int dataLength = 0;
+	int j = 0;
+
+  	ifstream products("src/../include/ProductDatabase.txt");
+  	if (products.is_open()) {
+		while(getline(products, line)) {
+			for (int i = 0; i < 4; i++) {
+				contentStartPos = line.find(contentSeparator, contentEndPos);
+				contentEndPos = line.find(contentSeparator, contentStartPos+1);
+				dataLength = contentEndPos - contentStartPos - 1;
+				data[i] = line.substr(contentStartPos+1, dataLength);
+				
+				pData[j].name  = (i == 0) ? data[i] : pData[j].name;
+				pData[j].value = (i == 1) ? stof(data[i]) : pData[j].value;
+				pData[j].stock = (i == 2) ? stoi(data[i]) : pData[j].stock;
+				pData[j].id  = (i == 3)   ? stoi(data[i]) : pData[j].id;
+			}
+			//cout << pData[j].name << endl;
+			//cout << pData[j].value << endl;
+			//cout << pData[j].stock << endl;
+			//cout << pData[j].id << endl;
+			j++;
+			contentStartPos = 0;
+			contentEndPos = 0;
+		}
+    	products.close();
+  	}
+  	//TODO: Use the log system
+  	else cout << "Unable to open file" << endl;
+}
+
+void Product::setProductDatabase(Product* t_product, std::vector<productInfo> &pData) {
+	for (int i = Product::MAX_VM_SLOTS-1; i >= 0; i--) {
+		
+		productInfo pDataTemp = pData.back(); 
+		pData.pop_back();
+
+		t_product[i].setName(pDataTemp.name);
+		t_product[i].setValue(pDataTemp.value);
+		t_product[i].setStock(pDataTemp.stock);
+		t_product[i].setId((ProductID)pDataTemp.id);
+
+		//std::cout << t_product[i].getName() << std::endl;
+		//std::cout << t_product[i].getValue() << std::endl;
+		//std::cout << t_product[i].getStock() << std::endl;
+		//std::cout << t_product[i].getId() << std::endl;		
+	}
 }
