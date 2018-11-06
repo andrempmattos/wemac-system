1d87a413211f15520e75c40b31371328c5cf6d9a
Unknown <andrempmattos@gmail.com>
2018-11-06 03:23:24 -0200

    Add the abstract interface class

---
+++ src/Interface.cpp
@@ -0,0 +1,7 @@
+#include "Interface.hpp"
+
+using namespace VMCore;
+
+Interface::Interface() {
+}
+
--- src/Interface.hpp
+++ src/Interface.hpp
@@ -1,13 +1,39 @@
 #ifndef VMCORE_INTERFACE_HPP
 #define VMCORE_INTERFACE_HPP
 
+#include <string>
+#include <iostream>
+
 namespace VMCore {
+    struct SystemData {
+    	std::string systemInput = "";
+    	std::string systemOutput = "";     
+    };
+
+    struct UserData {
+    	std::string userInput = "";
+    	std::string userOutput = "";  
+    };
+
+    struct AdvertisingData {
+    	std::string advertisingInput = "";
+    	std::string advertisingOutput = "";  
+    };
+
 	class Interface {
-		private:
-			
 		public:
 			Interface();
-			~Interface();	
+			virtual ~Interface() {}
+
+		public:
+			virtual void getSystemInput(SystemData* = nullptr) = 0;
+			virtual void setSystemOutput(SystemData* = nullptr) = 0;
+			
+			virtual void getUserInput(UserData* = nullptr) = 0;
+			virtual void setUserOutput(UserData* = nullptr) = 0;
+
+			virtual void printAdvertising(AdvertisingData* = nullptr) = 0;
+			virtual void insertAdvertising(AdvertisingData* = nullptr) = 0;
 	};
 }
 
